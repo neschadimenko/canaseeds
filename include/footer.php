abe617a7483d8ad9ed0f97432be1b40a3dce6338
neschadimenko <m.neschadimenko@ingenum.ua>
2015-08-06 14:17:45 +0300

    подвал

--- include/footer.php
+++ include/footer.php
@@ -9,37 +9,42 @@
 <li>
 <h3>Магазин</h3>
 </li>
-<li><a href="contracts.html" >Официально в Украине</a></li>
-<li> <a href="contracts.html" >Акции</a> </li>
-<li><a href="contracts.html" >Новости</a></li>
-<li><a href="contracts.html" >Контакты</a></li>
+<li><a href="/about-us/" >Официально в Украине</a></li>
+<li> <a href="discounts.php" >Акции</a> </li>
+<li><a href="news.php" >Новости</a></li>
+<li><a href="/contact-us/" >Контакты</a></li>
 </ul>
 <ul>
 <li>
-<h3>График работы</h3>
+<h3>Наш сервис</h3>
 </li>
-<li>Контакты</li>
-<li>Реквизиты</li>
-<li>Адрес</li>
+<li><a href="/oplata/" >Оплата</a></li>
+<li><a href="/dostavka/" >Доставка</a></li>
+<li><a href="/garantii/" >Гарантии качества</a></li>
+<li><a href="grafic.php" >График роботы</a></li>
 </ul>
 <ul>
 <li>
-<h3>Условия работы</h3>
+<h3>Информация</h3>
 </li>
-<li>Контакты</li>
-<li>Реквизиты</li>
-<li>Адрес</li>
+<li><a href="law.php" >Законодательство</a></li>
+<li><a href="trust.php" >Ответственность</a></li>
 </ul>
 <ul>
 <li>
 <h3>Помощь</h3>
 </li>
-<li>Контакты</li>
-<li>Реквизиты</li>
-<li>Адрес</li>
+<li><a href="order-help.php" >Как оформить заказ</a></li>
+<li><a href="FAQ.php" >Частые вопросы</a></li>
+<li><a href="otzuvu.php" >Рекомендации и отзывы</a></li>
 </ul> 
              </div>
         </td>
+        <td class="olk"><a href="https://olkpeace.org/forum/fpost3891791.html#p3891791" target="_blank">
+                <img src="https://olkpeace.org/olk_btn_bigger.png" alt="Общество Любителей Конопли" title="Общество Любителей Конопли" width="120"/></a>
+        </td>
+        <td class="trava"><a href="http://www.weedy.be/1328-ukraine-pub" target="_blank">
+                <img src="http://can.sysint.net/media/wysiwyg/jornal/j_2.png" alt="Журнал Трава" title="Журнал Трава" width="120"/></a></td>
         <td class="right">
             <div class="bottom-contacts">
                 <ul>
@@ -82,7 +87,13 @@
     <tr>
         <td class="left">
             <div class="footer-block-logos">
-                <img alt="" title="" src="/bitrix/templates/.default/images/credit_card_logos_13.gif" height="35" width="175" border="0"> 
+                <img alt="ПриватБанк" title="" src="/bitrix/templates/.default/images/pb.png" />
+                <img alt="Вебмани" title="" src="/bitrix/templates/.default/images/wm.png" />
+                <img alt="Виза" title="" src="/bitrix/templates/.default/images/visa.png" />
+                <img alt="МастерКард" title="" src="/bitrix/templates/.default/images/mc.png"/>
+                <img alt="Нова Пошта" title="" src="/bitrix/templates/.default/images/np1.png" />
+                <img alt="УкрПошта" title="" src="/bitrix/templates/.default/images/ukr.png" />
+                <img alt="Сертификат Thawte" title="" src="/bitrix/templates/.default/images/tw.png" />
             </div>
         </td>
         <td class="right">
